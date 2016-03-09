3a37d5d87890955160044234af1540880455595f
Derek Gonyeo <derek@gonyeo.com>
2016-03-08 19:36:03 -0500

    xmonad: added xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE NamedFieldPuns #-}
 import XMonad
 import XMonad.Layout.NoBorders
 import XMonad.Actions.GridSelect
 import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
 
 main :: IO ()
-main = do
-  xmonad $ defaultConfig
+main = xmonad =<< statusBar "xmobar" pp hideStat conf
+
+conf = defaultConfig
     { terminal    = "urxvt"
     , modMask     = mod4Mask
     , borderWidth = 1
@@ -15,13 +18,15 @@ main = do
     }
     `additionalKeysP`
     [ ("M-g", goToSelected defaultGSConfig)
-    , ("M-s", spawnSelected defaultGSConfig ["chromium", "evince", "openttd", "gimp", "xscreensaver-demo"])
     , ("M-x", spawn "xscreensaver-command -lock")
-    , ("M-<Up>", spawn "light -A 10")
-    , ("M-<Down>", spawn "light -U 10")
+    , ("M-s", spawnSelected defaultGSConfig ["chromium", "evince", "openttd", "gimp", "xscreensaver-demo", "pavucontrol", "wpa_gui"])
     , ("M-<Print>", spawn "scrot '/home/derek/screenshots/%Y-%m-%d_%T_$wx$h_scrot.png'")
     , ("M-<F1>", spawn "xrandr --output LVDS1 --rotate normal")
     , ("M-<F2>", spawn "xrandr --output LVDS1 --rotate left")
     , ("M-<F3>", spawn "xrandr --output LVDS1 --rotate inverted")
     , ("M-<F4>", spawn "xrandr --output LVDS1 --rotate right")
     ]
+
+pp = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+hideStat XConfig { modMask } = (modMask, xK_a)
