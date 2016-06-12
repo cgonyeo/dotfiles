538d85e7fb3b6fed839a71f76b28eb0d8a130592
Derek Gonyeo <derek.gonyeo@coreos.com>
2016-06-11 19:14:26 -0700

    added a check to my install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
 set -e
+
+echo "if your name is not Derek Gonyeo, you probably don't want to run this script"
+echo "it messes with more than just my vimrc"
+read -p "Are you sure? [y/N] " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 
 git submodule init
 git submodule update
