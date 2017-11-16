828e96bc8f532d7b72984d54fce7ab7aeb32bc5f
Derek Gonyeo <derek@gonyeo.com>
2017-11-15 16:45:58 -0800

    xmobar stuff

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -19,12 +19,21 @@ conf = defaultConfig
     `additionalKeysP`
     [ ("M-g", goToSelected defaultGSConfig)
     , ("M-x", spawn "xscreensaver-command -lock")
-    , ("M-s", spawnSelected defaultGSConfig ["chromium", "evince", "openttd", "gimp", "xscreensaver-demo", "pavucontrol", "wpa_gui"])
+    , ("M-s", spawnSelected defaultGSConfig [ "firefox"
+                                            , "gimp"
+                                            , "xscreensaver-demo"
+                                            , "pavucontrol"
+                                            , "spotify --force-device-scale-factor=2.0"
+                                            , "dwarf-fortress"
+                                            , "darktable"
+                                            , "chromium --profile-directory=Default --app-id=bikioccmkafdpakkkcpdbppfkghcmihk"
+                                            , "evince"
+                                            ])
     , ("M-<Print>", spawn "scrot '/home/derek/screenshots/%Y-%m-%d_%T_$wx$h_scrot.png'")
-    , ("M-<F1>", spawn "xrandr --output LVDS1 --rotate normal")
-    , ("M-<F2>", spawn "xrandr --output LVDS1 --rotate left")
-    , ("M-<F3>", spawn "xrandr --output LVDS1 --rotate inverted")
-    , ("M-<F4>", spawn "xrandr --output LVDS1 --rotate right")
+    , ("M-m", spawn "xdotool mousemove 1600 900")
+    , ("M-]", spawn "/home/derek/.bin/brightness_up")
+    , ("M-[", spawn "/home/derek/.bin/brightness_down")
+    , ("M-f", spawn "/usr/sbin/alsactl restore 0 -f ~/.alsaconfig-fix-static")
     ]
 
 pp = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
